f0d6005855249b0ccca325d2915bc0a9383a9137
AbdullahSh20 <abdullah.shammout@hotmail.com>
2024-01-29 13:29:40 +0300

    Add under construction page

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,12 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
-import HomePage from './pages/HomePage';
-import ProjectsPage from './pages/ProjectsPage';
-import ExperiencesPage from './pages/ExperiencesPage';
-import EducationPage from './pages/EducationPage';
-import Navbar from './components/Navbar';
+import ConstructionIcon from './ConstructionIcon';
 
 function App() {
   return (
-    <div className='app flex flex-col min-h-screen'>
-      <Router>
-        <Navbar />
-        <div className="flex-grow flex-shrink">
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/projects" element={<ProjectsPage />} />
-            <Route path="/experiences" element={<ExperiencesPage />} />
-            <Route path="/education" element={<EducationPage />} />
-          </Routes>
-        </div>
-      </Router>
-      <footer className="flex items-center h-16 px-4 border-t md:px-6">
-        <p className="text-xs text-gray-500 dark:text-gray-400">© 2024 Portfolio. All rights reserved.</p>
-      </footer>
+    <div className="flex flex-col items-center justify-center h-screen space-y-4">
+      <ConstructionIcon />
+      <h1 className="text-2xl font-bold">This site is undergoing an update</h1>
+      <p>Please check back later for updates.</p>
     </div>
   );
 }
