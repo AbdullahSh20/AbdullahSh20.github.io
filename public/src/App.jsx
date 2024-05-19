186d3843249221b858e2d12944954173902d88ab
AbdullahSh20 <abdullah.shammout@hotmail.com>
2024-05-19 17:06:29 +0300

    add a bunch of stuff related to blogging

--- public/src/App.jsx
+++ public/src/App.jsx
@@ -8,6 +8,11 @@ import ExperiencesPage from './pages/ExperiencesPage';
 import EducationPage from './pages/EducationPage';
 import BlogPage from './pages/BlogPage';
 import ActivitiesPage from './pages/ActivitiesPage';
+import CareerBlog from './pages/CareerBlog';
+import BooksBlog from './pages/BooksBlog';
+import GymBlog from './pages/GymBlog';
+import OtherBlog from './pages/OtherBlog';
+import AllBlog from './pages/AllBlog';
 
 function App() {
     return (
@@ -21,6 +26,11 @@ function App() {
             <Route path="/experiences" element={<ExperiencesPage />} />
             <Route path="/education" element={<EducationPage />} />
             <Route path="/blog" element={<BlogPage />} />
+            <Route path="/blog/Career" element={<CareerBlog />} />
+            <Route path="/blog/Books" element={<BooksBlog />} />
+            <Route path="/blog/Gym" element={<GymBlog />} />
+            <Route path="/blog/Other" element={<OtherBlog />} />
+            <Route path="/blog/all" element={<AllBlog />} />
             <Route path="/activities" element={<ActivitiesPage />} />
             <Route path="*" element={<Navigate to="/" />} />
           </Routes>
